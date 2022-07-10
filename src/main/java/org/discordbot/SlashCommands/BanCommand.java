da8566e01403ec165e5c5fc4488345a346766d83
MunJitso <72764494+MunJitso@users.noreply.github.com>
2022-07-10 13:54:30 +0100

    Updating Commands.

--- src/main/java/org/discordbot/SlashCommands/BanCommand.java
+++ src/main/java/org/discordbot/SlashCommands/BanCommand.java
@@ -3,24 +3,20 @@ package org.discordbot.SlashCommands;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 public class BanCommand extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        // Ban
         if ("ban".equals(event.getName())) {
-            Guild guild = event.getGuild();
-            assert guild != null;
             Member author = event.getMember();
             assert author != null;
             if (author.hasPermission(Permission.BAN_MEMBERS, Permission.ADMINISTRATOR)) {
+                Guild guild = event.getGuild();
+                assert guild != null;
                 try {
                     Member member = event.getOptionsByType(OptionType.MENTIONABLE).get(0).getAsMember();
                     assert member != null;
@@ -32,26 +28,7 @@ public class BanCommand extends ListenerAdapter {
                     guild.ban(member, 0).queue();
                 }
                 event.reply("banned.").setEphemeral(true).queue();
-
-                // Unban
-            } else if ("unban".equals(event.getName())) {
-                try {
-                    String id = event.getOptionsByType(OptionType.STRING).get(0).getAsString();
-                    List<Guild.Ban> banList = guild.retrieveBanList().complete();
-                    boolean isInList = false;
-                    for (Guild.Ban ban : banList)
-                        if (ban.getUser().getId().equals(id)) {
-                            isInList = true;
-                        }
-                    if (isInList) {
-                        guild.unban(User.fromId(id)).queue();
-                        event.reply("Unbanned").setEphemeral(true).queue();
-                    } else event.reply("you have to unban someone who's banned from the server..").setEphemeral(true).submit();
-                } catch (IndexOutOfBoundsException | NumberFormatException err) {
-                    event.reply("use Id to unban the target. USE DEVELOPER MODE...").setEphemeral(true).queue();
-                }
-
-            }
-        } else event.reply("shut up, you don't have permission.").setEphemeral(true).submit();
+            } else event.reply("shut up, you don't have permission.").setEphemeral(true).submit();
+        }
     }
 }

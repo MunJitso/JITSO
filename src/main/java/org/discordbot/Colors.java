2dc337d5cc0b8a2ac5494fb92259a4fe9d14ab2e
MunJitso <72764494+MunJitso@users.noreply.github.com>
2022-07-11 10:38:27 +0100

    Updating.

--- src/main/java/org/discordbot/Colors.java
+++ src/main/java/org/discordbot/Colors.java
@@ -9,11 +9,14 @@ public enum Colors {
     Orange(new Color(255, 214, 165)),
     Yellow(new Color(253, 255, 182)),
     Green(new Color(202, 255, 191)),
+    DarkGreen(new Color(88, 129, 87)),
     Cyan(new Color(155, 246, 255)),
     Blue(new Color(160, 196, 255)),
     Mauve(new Color(189, 178, 255)),
+    Purple(new Color(190, 137, 251)),
     Pink(new Color(255, 198, 255)),
     Brown(new Color(212, 163, 115)),
+    AWeirdColor(new Color(181, 162, 173)),
     White(new Color(255, 255, 252));
 
 
---
+++ src/main/java/org/discordbot/SlashCommands/TicketCommand.java
@@ -0,0 +1,53 @@
+package org.discordbot.SlashCommands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.EnumSet;
+
+public class TicketCommand extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if("ticket".equals(event.getName())){
+            Guild guild = event.getGuild();
+            assert guild != null;
+            Member bot = guild.getMemberById("878037797355798589");
+            assert bot != null;
+            EmbedBuilder message = new EmbedBuilder();
+            message.setColor(new Color(88, 129, 87))
+                    .setTitle("Open a Ticket!")
+                    .setDescription("Click on the button below to create a ticket.")
+                    .setFooter(bot.getUser().getAsTag(),bot.getAvatarUrl());
+            event.replyEmbeds(message.build())
+                    .addActionRow(
+                            Button.secondary("ticketCreation", "Create Ticket.")
+                    ).submit();
+        }
+    }
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        if(event.getComponentId().equals("ticketCreation")){
+            Guild guild = event.getGuild();
+            User user = event.getUser();
+            assert guild != null;
+            Member member = guild.getMemberById(user.getId());
+            assert member != null;
+            TextChannel channel = guild.getTextChannelsByName(String.format("ticket-%s", user.getId()), false).get(0);
+            assert channel != null;
+            EnumSet<Permission> permissions = EnumSet.of(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND);
+            channel.getManager().putPermissionOverride(member, permissions, null).queue();
+            event.reply("Created.").setEphemeral(true).submit();
+        }
+    }
+}

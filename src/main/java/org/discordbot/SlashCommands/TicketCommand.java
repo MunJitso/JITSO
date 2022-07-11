d19d4517dab24178584b24ef6aa5ae1383738e6e
MunJitso <72764494+MunJitso@users.noreply.github.com>
2022-07-11 21:33:04 +0100

    Update TicketCommand.java

--- src/main/java/org/discordbot/SlashCommands/TicketCommand.java
+++ src/main/java/org/discordbot/SlashCommands/TicketCommand.java
@@ -53,7 +53,7 @@ public class TicketCommand extends ListenerAdapter {
                         .addMemberPermissionOverride(user.getIdLong(), permissions, null)
                         .queue();
                 TextChannel channel = guild.getTextChannelsByName(String.format("ticket-%s", user.getId()), false).get(0);
-                event.reply("Created. " + channel.getAsMention()).setEphemeral(true).queue();
+                event.reply(String.format("Created, %s", channel.getAsMention())).setEphemeral(true).submit();
             }
 
         }
